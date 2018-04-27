e2334b21d16c9d3520fbb0d8f7311a7d162a3d84
Shawn Nolan <shawn@shawnnolan.com>
2018-04-27 00:03:48 -0700

    - Added some firebase configs to test out with.

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDZ3paicHLtb2VMmGN75f4u-5qiEnoeMrQ",
+  authDomain: "ng-shawnnolan.firebaseapp.com",
+  databaseURL: "https://ng-shawnnolan.firebaseio.com",
+  projectId: "ng-shawnnolan",
+  storageBucket: "ng-shawnnolan.appspot.com",
+  messagingSenderId: "849970264446"
+};
+firebase.initializeApp(config);
